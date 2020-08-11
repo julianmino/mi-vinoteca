be5937056bada186b34d8d5ef5e40422fd138650
julianmino <m99_julian@hotmail.com>
2020-08-10 22:01:45 -0300

    deleted Business.Entities, Data.Database & added DAL, ClienteLogic

--- Business.Logic/ClienteLogic.cs
+++ Business.Logic/ClienteLogic.cs
@@ -3,42 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Business.Entities;
-using Data.Database;
+using DAL;
 
 namespace Business.Logic
 {
     public class ClienteLogic:BusinessLogic
     {
-
-        private Data.Database.ClienteAdapter _UsuarioData;
-
-        public ClienteAdapter ClienteData { get => _UsuarioData; set => _UsuarioData = value; }
-
-
-        public ClienteLogic(Data.Database.ClienteAdapter clientedata)
+        private YaguaronEntities context = new YaguaronEntities();
+        public ClienteLogic()
         {
-            ClienteData = clientedata;
         }
 
-        public List<Cliente> GetAll()
+        public List<cliente> GetAll()
         {
-            List<Cliente> clientes = ClienteData.GetAll();
-            return clientes;
+            List<cliente> listaClientes = context.clientes.ToList();
+
+            return listaClientes;
         }
-        public Cliente GetOne(int id)
+        public cliente GetOne(int id)
         {
-            Cliente cliente = ClienteData.GetOne(id);
+            cliente cliente = context.clientes.SingleOrDefault(x => x.id_cliente == id);
+
             return cliente;
         }
-        public void Delete(int id)
+        public static int Alta(string nom, string ape, string usuario, 
+            string email, string clave, DateTime fecha_nac,bool premium, int id_descuento)
         {
-            ClienteData.Delete(id);
+
+            using (var context = new YaguaronEntities())
+            {
+                try
+                {
+                    var cliente = new cliente()
+                    {
+                        nombre = nom,
+                        apellido = ape,
+                        usuario = usuario,
+                        email = email,
+                        clave = clave,
+                        fecha_nac = fecha_nac,
+                        premium = premium,
+                        id_descuento = id_descuento
+                    };
+                    context.clientes.Add(cliente);
+                    context.SaveChanges();
+
+                    return cliente.id_cliente;
+                }
+                catch (Exception Ex)
+                {
+                    Console.WriteLine(Ex.InnerException?.Message);
+                    return 0;
+                }
+            }
+        }
+                
+        public static void Modificacion(int ID, string nueva_clave)
+        {
+            using (var context = new YaguaronEntities())
+            {
+                cliente clienteAActualizar = context.clientes.Find(ID);
+                clienteAActualizar.clave = nueva_clave;
+                context.SaveChanges();
+            }
         }
 
-        public void Save(Cliente cliente)
+        public static void Baja(int ID)
         {
-            ClienteData.Save(cliente);
+            using (var context = new YaguaronEntities())
+            {
+                cliente clienteAEliminar = context.clientes.Find(ID);
+                if (clienteAEliminar != null)
+                {
+                    context.clientes.Remove(clienteAEliminar);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public static void Consulta(string filtro)
+        {
+            using (var context = new YaguaronEntities())
+            {
+                foreach (var cliente in context.clientes.Where(u => u.usuario.Contains(filtro)))
+                {
+                    Console.WriteLine(cliente.usuario);
+                }
+            }
         }
 
     }
---
+++ DAL/lineas_pedidos.cs
@@ -0,0 +1,29 @@
+namespace DAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class lineas_pedidos
+    {
+        [Key]
+        [Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int id_pedido { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int id_producto { get; set; }
+
+        public int cantidad { get; set; }
+
+        public double subtotal { get; set; }
+
+        public virtual producto producto { get; set; }
+
+        public virtual pedido pedido { get; set; }
+    }
+}
